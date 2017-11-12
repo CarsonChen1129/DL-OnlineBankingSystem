45af0859ea2b8fdfdd4e6d88fce619cbdf075cff
SenWangHost <senwang1992010@gmail.com>
2017-11-12 17:49:21 -0500

    Add animation with enlarge effect to the button in the footer component

--- DL-OBS/src/app/footer/footer.component.ts
+++ DL-OBS/src/app/footer/footer.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {trigger,state,style,animate,transition} from '@angular/animations';
 
 @Component({
   selector: 'footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  // made a new animations here
+  animations: [
+    trigger('state', [
+      state('inactive', style({
+        backgroundColor:'#b79344',
+        transform: 'scale(1)'
+      })),
+      state('active',   style({
+        backgroundColor:'#f4bd0c',
+        transform: 'scale(1.05)'
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class FooterComponent implements OnInit {
+
+  state:string = 'inactive';
   constructor() { }
 
   ngOnInit() {
   }
-
+  toggleState() {
+    if (this.state == 'inactive') {
+      this.state = 'active';
+    } else {
+      this.state = 'inactive';
+    }
+  }
 }
