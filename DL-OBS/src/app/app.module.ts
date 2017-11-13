0e16772b1f98107e1a09e40788748a440f66238b
carsonchen1129@gmail.com <carsonchen1129@gmail.com>
2017-11-13 15:02:36 -0500

    Revert "Initialize a branch for header and footer."
    This reverts commit a0849a47bfa8439d6ac95c3b84cd7e48f7320c30.

--- DL-OBS/src/app/app.module.ts
+++ DL-OBS/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import {WelcomePageComponent} from '../pages/welcome-page/welcome-page';
 import {LoginComponent} from '../pages/login/login';
 import {RegisterComponent} from '../pages/register/register';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -15,13 +13,10 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     WelcomePageComponent,
     LoginComponent,
-    RegisterComponent,
-    HeaderComponent,
-    FooterComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- DL-OBS/src/pages/welcome-page/welcome-page.ts
+++ DL-OBS/src/pages/welcome-page/welcome-page.ts
@@ -1,9 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-    selector:'page-welcome',
-    templateUrl:'welcome-page.html',
-    styleUrls: ['welcome-page.scss']
+  selector:'page-welcome',
+  templateUrl:'welcome-page.html'
 })
 export class WelcomePageComponent implements OnInit {
 
