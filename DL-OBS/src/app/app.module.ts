b79e6781fd0db857f96e6abba73a0d88539898f4
SenWangHost <senwang1992010@gmail.com>
2017-11-13 16:09:47 -0500

    Merge header-footer into login-register

--- DL-OBS/src/app/app.module.ts
+++ DL-OBS/src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
 import {WelcomePageComponent} from '../pages/welcome-page/welcome-page';
@@ -10,6 +16,13 @@ import {RegisterComponent} from '../pages/register/register';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAkv3jLcTIQ1APk1e0BFAW70e_ndHbWrbM',
+  authDomain: 'fir-2bd19.firebaseapp.com',
+  databaseURL: 'https://fir-2bd19.firebaseio.com',
+  storageBucket: 'fir-2bd19.appspot.com',
+  messagingSenderId: '932306096637'
+};
 
 @NgModule({
   declarations: [
@@ -23,9 +36,15 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
