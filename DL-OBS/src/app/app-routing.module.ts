b833359c039e9447e70c2bf248e3bb90fa8b48ac
SenWangHost <senwang1992010@gmail.com>
2017-11-27 16:55:17 -0500

    Set up the routing for three transfer fund pages

--- DL-OBS/src/app/app-routing.module.ts
+++ DL-OBS/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import {WelcomePageComponent} from '../pages/welcome-page/welcome-page';
 import {DashboardComponent} from '../pages/dashboard/dashboard';
 import {RegisterComponent} from "../pages/register/register.component";
 import {AccountpageComponent} from "../pages/account-page/account-page";
+import {TfDashboardComponent} from '../pages/transfer-fund/tf-dashboard/tf-dashboard.component';
+import {TfInternalTransferComponent} from '../pages/transfer-fund/tf-internal-transfer/tf-internal-transfer.component';
+import {TfCheckDepositComponent} from '../pages/transfer-fund/tf-check-deposit/tf-check-deposit.component';
+import {TfPayOthersComponent} from '../pages/transfer-fund/tf-pay-others/tf-pay-others.component';
 
 
 const routes: Routes = [
@@ -22,6 +26,22 @@ const routes: Routes = [
     {
         path: 'account',
         component: AccountpageComponent
+    },
+    {
+        path: 'transfer-fund/dashboard',
+        component: TfDashboardComponent
+    },
+    {
+        path: 'transfer-fund/internal-transfer',
+        component: TfInternalTransferComponent
+    },
+    {
+        path: 'transfer-fund/check-deposit',
+        component: TfCheckDepositComponent
+    },
+    {
+        path: 'transfer-fund/pay-others',
+        component: TfPayOthersComponent
     }
 ];
 
