fdce2734fae75baf5254d8bb86495dde85796595
SenWangHost <senwang1992010@gmail.com>
2017-11-29 21:42:22 -0500

    Finish implementing the pay-others page.

--- DL-OBS/src/pages/transfer-fund/tf-pay-others/tf-pay-others.component.ts
+++ DL-OBS/src/pages/transfer-fund/tf-pay-others/tf-pay-others.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 @Component({
   selector: 'app-tf-pay-others',
@@ -7,10 +8,19 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class TfPayOthersComponent implements OnInit {
-
+  note:string = '';
+  hide:boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+  hideform() {
+    if (this.hide) {
+      this.hide = false;
+      
+    } else {
+      this.hide = true;
+    }
   }
 
 }
