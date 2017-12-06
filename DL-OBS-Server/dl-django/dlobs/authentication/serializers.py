bc149fd63622a99957572be5ea12f8908f020f5f
carsonchen1129@gmail.com <carsonchen1129@gmail.com>
2017-12-06 16:03:24 -0500

    1. Switch from JEE development to Django development  2. Made Django RESTful.  3. Implemented the basic authentication (Create Account/Login/Logout)
    1. Switch from JEE development to Django development  2. Made Django
    RESTful.  3. Implemented the basic authentication (Create
    Account/Login/Logout)

---
+++ DL-OBS-Server/dl-django/dlobs/authentication/serializers.py
@@ -0,0 +1,77 @@
+from django.contrib.auth import update_session_auth_hash
+from rest_framework import serializers
+from models import Account
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    print "In AccountSerializer"
+    password = serializers.CharField(write_only=True, required=False)
+    confirm_password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        print "In AccoutSerializer meta"
+        model = Account
+        fields = (
+            'id',
+            'email',
+            'first_name',
+            'middle_name',
+            'last_name',
+            'dob',
+            'primary_phone_number',
+            'alternative_phone_number',
+            'home_address_1',
+            'home_address_2',
+            'home_address_city',
+            'home_address_state',
+            'home_address_country',
+            'home_address_zip',
+            'mail_address_1',
+            'mail_address_2',
+            'mail_address_city',
+            'mail_address_state',
+            'mail_address_country',
+            'mail_address_zip',
+            'license_number',
+            'license_type',
+            'license_issue_date',
+            'license_expire',
+            'license_geo',
+            'occupation',
+            'annual_income',
+            'employee',
+            'security_question_1',
+            'security_answer_1',
+            'security_question_2',
+            'security_answer_2',
+            'security_question_3',
+            'security_answer_3',
+            'language',
+            'created_at',
+            'updated_at',
+            'password',
+            'confirm_password',)
+        read_only_fields = ('created_at', 'updated_at',)
+
+        def create(self, validated_data):
+            print "In AccountSerializer create"
+            print validated_data
+            return Account.objects.create(**validated_data)
+
+        def update(self, instance, validated_data):
+            print "In AccountSerializer update"
+            instance.email = validated_data.get('email', instance.email)
+            instance.first_name = validated_data.get('first_name', instance.first_name)
+
+            instance.save()
+
+            password = validated_data.get('password', None)
+            confirm_password = validated_data.get('confirm_password', None)
+
+            if password and confirm_password and password == confirm_password:
+                instance.set_password(password)
+                instance.save()
+
+            update_session_auth_hash(self.context.get('request'), instance)
+
+            return instance
---
+++ DL-OBS-Server/dl-django/dlobs/dlobs/urls.py
@@ -0,0 +1,28 @@
+"""dlobs URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from rest_framework import routers
+from authentication.views import AccountViewSet, LoginView, LogoutView
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
+urlpatterns = [
+    url(r'', include(router.urls)),
+    url(r'^auth/login', LoginView.as_view(), name='login'),
+    url(r'^auth/logout', LogoutView.as_view(), name='logout'),
+]
