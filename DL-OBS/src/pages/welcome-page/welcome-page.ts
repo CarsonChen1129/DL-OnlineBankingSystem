c2be25e4cb4b20d0e514e831852aac29ce63ddc6
SenWangHost <senwang1992010@gmail.com>
2017-12-05 21:54:02 -0500

    Finish implementing the account page.

--- DL-OBS/src/pages/welcome-page/welcome-page.ts
+++ DL-OBS/src/pages/welcome-page/welcome-page.ts
@@ -14,7 +14,7 @@ import {MatSnackBar} from "@angular/material";
 export class WelcomePageComponent implements OnInit {
 
   products = [
-    {title: 'Let us hep you get on the road', description: 'Take a few minutes to apply online to get your loan approved before you go to the dealer. You are just a few steps away from your new car.', img: 'assets/images/auto-loan.jpg'},
+    {title: 'Let us help you get on the road', description: 'Take a few minutes to apply online to get your loan approved before you go to the dealer. You are just a few steps away from your new car.', img: 'assets/images/auto-loan.jpg'},
     {title: 'Making a home change?', description: 'Start a relationship with a Home Lending Advisor to personalize a plan that\'s right for you.', img: 'assets/images/mortgage2.jpg'},
     {title: 'Tap into your home\'s equity', description: 'Help finance what\'s important with a home equity line of credit.', img: 'assets/images/home-equity.jpg'},
     {title: 'Devonshire Mobile Banking', description: 'The Latest mobile banking app built around you. It\'s simple, rewarding, and secure.', img: 'assets/images/dl-mobile.png'}
